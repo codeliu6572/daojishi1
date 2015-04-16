3dd80e87805a4c21f7d035cbbbf8d4721a97e6b9
yons <yons@yonsdeMac-Pro.local>
2015-04-16 15:42:59 +0800

    Initial Commit

---
+++ 倒计时/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  倒计时
+//
+//  Created by yons on 15/4/16.
+//  Copyright (c) 2015年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 倒计时/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  倒计时
+//
+//  Created by yons on 15/4/16.
+//  Copyright (c) 2015年 yons. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
